f78f8aca42a036ab695dbee499942203194d31b3
k2ghostyou <keengkhalid@gmail.com>
2026-06-19 11:38:16 +0000

    feat: wire up public package exports

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from "./types";
+export * from "./translate";
+export * from "./formatters/amount";
+export * from "./formatters/date";
+export * from "./rtl/rtl";
+export * from "./react/I18nProvider";
+export { LOCALE_METADATA } from "./locales/registry";
